c7a70684182addb69397104f384ec49805fa5a31
githubvanlea <112865265+githubvanlea@users.noreply.github.com>
2023-10-05 10:00:42 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
 console.log("hi");
+
+/* menu openen */
+var deOpenButton = document.querySelector("header > button"); 
+var hetMenu = document.querySelector("header nav");
+
+deOpenButton.onclick = openNav;
+
+function openNav() {
+    hetMenu.classList.add("open");
+  }
+
+/* menu sluiten */
+var deSluitButton = document.querySelector("nav button");
+
+deSluitButton.onclick = sluitMenu;
+
+function sluitMenu() {
+  hetMenu.classList.remove("open");
+}
